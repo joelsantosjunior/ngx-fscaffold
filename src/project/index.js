b6e189ac09d8582922a16750e8f03f11652a6d12
joelsantosjunior <joelsantosjunior@outlook.com>
2021-10-28 22:25:51 -0300

    att versão

--- src/project/index.js
+++ src/project/index.js
@@ -4,11 +4,13 @@ exports.generateProject = void 0;
 const schematics_1 = require("@angular-devkit/schematics");
 const generate_application_1 = require("../webcomponent/rules/generate-application");
 const add_single_spa_1 = require("./rules/add-single-spa");
+const add_files_1 = require("./rules/add-files");
 function generateProject(_options) {
     return (tree, _context) => {
         const opts = Object.assign(Object.assign({}, _options), { project: _options.name, routing: true });
         return schematics_1.chain([
             generate_application_1.generateApp(opts),
+            add_files_1.addFiles(opts),
             add_single_spa_1.addSingleSPAAngular(opts)
         ]);
     };
